ffa8278e36932e5b2f46f369948cbfbd62ecd85d
Daniel <105226307+Thoobi@users.noreply.github.com>
2024-05-26 15:39:41 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Fetch from './Fetch1';
+import './App.css'
+
+function App() {
+  return (
+    <div>
+    <Fetch />
+    </div>
+  )
+}
+
+export default App
